30233216d596033e7be4ac1deeeeda2cb09ae413
alamdeep-1 <alamdeep.1@gmail.com>
2017-11-05 00:40:36 +0530

    added play & pause funtionality

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-$('.input-wrapper button').on('click',function (e) {
+$('.input-wrapper form').on('submit',function (e) {
     e.preventDefault();
     $('.welcome-screen').addClass('hidden');
     var name = $('#name-input').val();
     $('.main .user-name').html('Welcome, ' + name);
     $('.main').removeClass('hidden');
+});
+
+$('.clickable').on('click', function () {
+    var audio = $('audio')[0];
+    if(audio.paused) {
+        audio.play();
+        $(this).removeClass('fa-play').addClass('fa-pause')
+    }
+    else {
+        audio.pause();
+        $(this).removeClass('fa-pause').addClass('fa-play')
+    }
 });
